bd8eafb8c83a52ddc0c887e6d0146346b290a3dd
AaaronHooooper <aaron.hooper@ms223.org>
2016-05-03 09:23:35 -0400

    Update Quiz and Review-Aaron.py

--- Quiz and Review-Aaron.py
+++ Quiz and Review-Aaron.py
@@ -1,7 +1,6 @@
 def max_value(integer):
-   print range (1,integer + 1)
-max_value(51)
-
+    print range (1, integer + 1)
+max_value(10)
 
 def compare_lists():
     list1 = [4,5,15,11,23,42]
@@ -12,3 +11,14 @@ def compare_lists():
         else:
             print list2[list1.index(x)]
 compare_lists()
+
+
+def swapping_stars():
+    height = 6
+    width = 3
+    for x in range (0, height):
+        if x % 2 == 1:
+            print "- * " * width
+        else:
+            print '* - ' * width
+swapping_stars()
