c2ed182041942753c987d6d0d00211def0dc1b72
AaaronHooooper <aaron.hooper@ms223.org>
2016-05-03 09:17:51 -0400

    Update Quiz and Review-Aaron.py

--- Quiz and Review-Aaron.py
+++ Quiz and Review-Aaron.py
@@ -1,3 +1,14 @@
-def Max_value(integer):
+def max_value(integer):
    print range (1,integer + 1)
-Max_value(51)
+max_value(51)
+
+
+def compare_lists():
+    list1 = [4,5,15,11,23,42]
+    list2 = [1,8,7,16,7,35]
+    for x in list1:
+        if x > list2[list1.index(x)]:
+            print x
+        else:
+            print list2[list1.index(x)]
+compare_lists()
