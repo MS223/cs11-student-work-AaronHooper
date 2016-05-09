e356c22f0b4cb0eaf03561e34d0876e5f85a7776
AaaronHooooper <aaron.hooper@ms223.org>
2016-05-09 08:56:17 -0400

    Create May 6 Do Now_StringSlicing.py

---
+++ May 6 Do Now_StringSlicing.py
@@ -0,0 +1,9 @@
+print "hello"[1:]
+print "hello"[:4]
+print "hello"[2:4]
+
+print "Github"[1:3]
+print "Hamburger"[3:]
+print "dongle"[1:3]
+print "snapchat" [1:4]
+#The code prints out a certain part of the whole word for example it of Github or buurger from hamburger
