1f7c2a4733c33baab3921283ba386c1ad538eb9e
AaaronHooooper <aaron.hooper@ms223.org>
2016-05-24 09:29:28 -0400

    Create ColorCoordinate.py

---
+++ ColorCoordinate.py
@@ -0,0 +1,18 @@
+class color(object):
+   """represent color"""
+
+DarkOrchid = color()
+DarkGoldenRod = color()
+DarkMagenta = color()
+
+DarkOrchid.r = 153
+DarkOrchid.g = 50
+DarkOrchid.b = 204
+
+DarkGoldenRod.r = 184
+DarkGoldenRod.g = 134
+DarkGoldenRod.b = 11
+
+DarkMagenta.r = 139
+DarkMagenta.g = 0
+DarkMagenta.b = 139
